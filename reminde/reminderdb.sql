127fbbdc2ee35f7e75e93924e0d12baf3ec65806
Zaghit-Prado <114609659+Zaghit-Prado@users.noreply.github.com>
2022-09-28 18:18:43 -0500

    Add files via upload

---
+++ reminde/reminderdb.sql
@@ -0,0 +1,652 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-09-2022 a las 01:17:34
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `reminderdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `marca_targeta`
+--
+
+CREATE TABLE `marca_targeta` (
+  `id_targ_mar` int(8) NOT NULL,
+  `marca` text NOT NULL,
+  `img_moneda` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `marca_targeta`
+--
+
+INSERT INTO `marca_targeta` (`id_targ_mar`, `marca`, `img_moneda`) VALUES
+(1, 'Visa', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEOeE8e3SvlJB1MnYXE0O6hnv6Pkxf5-gEq3lNUFbU291nBNnZ6_5P2caWB115BkzGJs8&usqp=CAU'),
+(2, 'Master Card', 'https://gestion.pe/resizer/fvNIiNV7G9Vicx089r-lMj-a-yg=/1200x900/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/IPKPYANGYZGI5GB3YSYY7YZNXM.jpg'),
+(3, 'American Express\r\n', 'https://brandemia.org/sites/default/files/inline/images/amex-logo-antes.jpg'),
+(4, 'Diners Club', 'https://play-lh.googleusercontent.com/N-OxShhJwd8IMK7XTtUysqOQNIwd0Gc9TZ7ZgGmSIBbt-t3Gb2XeKdWh03Ba-XzBgA');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `monedas`
+--
+
+CREATE TABLE `monedas` (
+  `id_moneda` int(8) NOT NULL,
+  `moneda` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `convertir` decimal(4,2) NOT NULL,
+  `simbolo` varchar(2) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `monedas`
+--
+
+INSERT INTO `monedas` (`id_moneda`, `moneda`, `convertir`, `simbolo`) VALUES
+(1, 'Sol', '1.00', 'S/'),
+(2, 'dolar', '3.80', '$');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notif`
+--
+
+CREATE TABLE `notif` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(155) COLLATE utf8_unicode_ci NOT NULL,
+  `correo` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `num` int(4) NOT NULL DEFAULT 1,
+  `color` text COLLATE utf8_unicode_ci NOT NULL DEFAULT '#c00101'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paises`
+--
+
+CREATE TABLE `paises` (
+  `id_pais` int(11) NOT NULL,
+  `iso` char(2) NOT NULL,
+  `pais` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `paises`
+--
+
+INSERT INTO `paises` (`id_pais`, `iso`, `pais`) VALUES
+(1, 'AF', 'Afganistán'),
+(2, 'AX', 'Islas Gland'),
+(3, 'AL', 'Albania'),
+(4, 'DE', 'Alemania'),
+(5, 'AD', 'Andorra'),
+(6, 'AO', 'Angola'),
+(7, 'AI', 'Anguilla'),
+(8, 'AQ', 'Antártida'),
+(9, 'AG', 'Antigua y Barbuda'),
+(10, 'AN', 'Antillas Holandesas'),
+(11, 'SA', 'Arabia Saudí'),
+(12, 'DZ', 'Argelia'),
+(13, 'AR', 'Argentina'),
+(14, 'AM', 'Armenia'),
+(15, 'AW', 'Aruba'),
+(16, 'AU', 'Australia'),
+(17, 'AT', 'Austria'),
+(18, 'AZ', 'Azerbaiyán'),
+(19, 'BS', 'Bahamas'),
+(20, 'BH', 'Bahréin'),
+(21, 'BD', 'Bangladesh'),
+(22, 'BB', 'Barbados'),
+(23, 'BY', 'Bielorrusia'),
+(24, 'BE', 'Bélgica'),
+(25, 'BZ', 'Belice'),
+(26, 'BJ', 'Benin'),
+(27, 'BM', 'Bermudas'),
+(28, 'BT', 'Bhután'),
+(29, 'BO', 'Bolivia'),
+(30, 'BA', 'Bosnia y Herzegovina'),
+(31, 'BW', 'Botsuana'),
+(32, 'BV', 'Isla Bouvet'),
+(33, 'BR', 'Brasil'),
+(34, 'BN', 'Brunéi'),
+(35, 'BG', 'Bulgaria'),
+(36, 'BF', 'Burkina Faso'),
+(37, 'BI', 'Burundi'),
+(38, 'CV', 'Cabo Verde'),
+(39, 'KY', 'Islas Caimán'),
+(40, 'KH', 'Camboya'),
+(41, 'CM', 'Camerún'),
+(42, 'CA', 'Canadá'),
+(43, 'CF', 'República Centroafricana'),
+(44, 'TD', 'Chad'),
+(45, 'CZ', 'República Checa'),
+(46, 'CL', 'Chile'),
+(47, 'CN', 'China'),
+(48, 'CY', 'Chipre'),
+(49, 'CX', 'Isla de Navidad'),
+(50, 'VA', 'Ciudad del Vaticano'),
+(51, 'CC', 'Islas Cocos'),
+(52, 'CO', 'Colombia'),
+(53, 'KM', 'Comoras'),
+(54, 'CD', 'República Democrática del Congo'),
+(55, 'CG', 'Congo'),
+(56, 'CK', 'Islas Cook'),
+(57, 'KP', 'Corea del Norte'),
+(58, 'KR', 'Corea del Sur'),
+(59, 'CI', 'Costa de Marfil'),
+(60, 'CR', 'Costa Rica'),
+(61, 'HR', 'Croacia'),
+(62, 'CU', 'Cuba'),
+(63, 'DK', 'Dinamarca'),
+(64, 'DM', 'Dominica'),
+(65, 'DO', 'República Dominicana'),
+(66, 'EC', 'Ecuador'),
+(67, 'EG', 'Egipto'),
+(68, 'SV', 'El Salvador'),
+(69, 'AE', 'Emiratos Árabes Unidos'),
+(70, 'ER', 'Eritrea'),
+(71, 'SK', 'Eslovaquia'),
+(72, 'SI', 'Eslovenia'),
+(73, 'ES', 'España'),
+(74, 'UM', 'Islas ultramarinas de Estados Unidos'),
+(75, 'US', 'Estados Unidos'),
+(76, 'EE', 'Estonia'),
+(77, 'ET', 'Etiopía'),
+(78, 'FO', 'Islas Feroe'),
+(79, 'PH', 'Filipinas'),
+(80, 'FI', 'Finlandia'),
+(81, 'FJ', 'Fiyi'),
+(82, 'FR', 'Francia'),
+(83, 'GA', 'Gabón'),
+(84, 'GM', 'Gambia'),
+(85, 'GE', 'Georgia'),
+(86, 'GS', 'Islas Georgias del Sur y Sandwich del Sur'),
+(87, 'GH', 'Ghana'),
+(88, 'GI', 'Gibraltar'),
+(89, 'GD', 'Granada'),
+(90, 'GR', 'Grecia'),
+(91, 'GL', 'Groenlandia'),
+(92, 'GP', 'Guadalupe'),
+(93, 'GU', 'Guam'),
+(94, 'GT', 'Guatemala'),
+(95, 'GF', 'Guayana Francesa'),
+(96, 'GN', 'Guinea'),
+(97, 'GQ', 'Guinea Ecuatorial'),
+(98, 'GW', 'Guinea-Bissau'),
+(99, 'GY', 'Guyana'),
+(100, 'HT', 'Haití'),
+(101, 'HM', 'Islas Heard y McDonald'),
+(102, 'HN', 'Honduras'),
+(103, 'HK', 'Hong Kong'),
+(104, 'HU', 'Hungría'),
+(105, 'IN', 'India'),
+(106, 'ID', 'Indonesia'),
+(107, 'IR', 'Irán'),
+(108, 'IQ', 'Iraq'),
+(109, 'IE', 'Irlanda'),
+(110, 'IS', 'Islandia'),
+(111, 'IL', 'Israel'),
+(112, 'IT', 'Italia'),
+(113, 'JM', 'Jamaica'),
+(114, 'JP', 'Japón'),
+(115, 'JO', 'Jordania'),
+(116, 'KZ', 'Kazajstán'),
+(117, 'KE', 'Kenia'),
+(118, 'KG', 'Kirguistán'),
+(119, 'KI', 'Kiribati'),
+(120, 'KW', 'Kuwait'),
+(121, 'LA', 'Laos'),
+(122, 'LS', 'Lesotho'),
+(123, 'LV', 'Letonia'),
+(124, 'LB', 'Líbano'),
+(125, 'LR', 'Liberia'),
+(126, 'LY', 'Libia'),
+(127, 'LI', 'Liechtenstein'),
+(128, 'LT', 'Lituania'),
+(129, 'LU', 'Luxemburgo'),
+(130, 'MO', 'Macao'),
+(131, 'MK', 'ARY Macedonia'),
+(132, 'MG', 'Madagascar'),
+(133, 'MY', 'Malasia'),
+(134, 'MW', 'Malawi'),
+(135, 'MV', 'Maldivas'),
+(136, 'ML', 'Malí'),
+(137, 'MT', 'Malta'),
+(138, 'FK', 'Islas Malvinas'),
+(139, 'MP', 'Islas Marianas del Norte'),
+(140, 'MA', 'Marruecos'),
+(141, 'MH', 'Islas Marshall'),
+(142, 'MQ', 'Martinica'),
+(143, 'MU', 'Mauricio'),
+(144, 'MR', 'Mauritania'),
+(145, 'YT', 'Mayotte'),
+(146, 'MX', 'México'),
+(147, 'FM', 'Micronesia'),
+(148, 'MD', 'Moldavia'),
+(149, 'MC', 'Mónaco'),
+(150, 'MN', 'Mongolia'),
+(151, 'MS', 'Montserrat'),
+(152, 'MZ', 'Mozambique'),
+(153, 'MM', 'Myanmar'),
+(154, 'NA', 'Namibia'),
+(155, 'NR', 'Nauru'),
+(156, 'NP', 'Nepal'),
+(157, 'NI', 'Nicaragua'),
+(158, 'NE', 'Níger'),
+(159, 'NG', 'Nigeria'),
+(160, 'NU', 'Niue'),
+(161, 'NF', 'Isla Norfolk'),
+(162, 'NO', 'Noruega'),
+(163, 'NC', 'Nueva Caledonia'),
+(164, 'NZ', 'Nueva Zelanda'),
+(165, 'OM', 'Omán'),
+(166, 'NL', 'Países Bajos'),
+(167, 'PK', 'Pakistán'),
+(168, 'PW', 'Palau'),
+(169, 'PS', 'Palestina'),
+(170, 'PA', 'Panamá'),
+(171, 'PG', 'Papúa Nueva Guinea'),
+(172, 'PY', 'Paraguay'),
+(173, 'PE', 'Perú'),
+(174, 'PN', 'Islas Pitcairn'),
+(175, 'PF', 'Polinesia Francesa'),
+(176, 'PL', 'Polonia'),
+(177, 'PT', 'Portugal'),
+(178, 'PR', 'Puerto Rico'),
+(179, 'QA', 'Qatar'),
+(180, 'GB', 'Reino Unido'),
+(181, 'RE', 'Reunión'),
+(182, 'RW', 'Ruanda'),
+(183, 'RO', 'Rumania'),
+(184, 'RU', 'Rusia'),
+(185, 'EH', 'Sahara Occidental'),
+(186, 'SB', 'Islas Salomón'),
+(187, 'WS', 'Samoa'),
+(188, 'AS', 'Samoa Americana'),
+(189, 'KN', 'San Cristóbal y Nevis'),
+(190, 'SM', 'San Marino'),
+(191, 'PM', 'San Pedro y Miquelón'),
+(192, 'VC', 'San Vicente y las Granadinas'),
+(193, 'SH', 'Santa Helena'),
+(194, 'LC', 'Santa Lucía'),
+(195, 'ST', 'Santo Tomé y Príncipe'),
+(196, 'SN', 'Senegal'),
+(197, 'CS', 'Serbia y Montenegro'),
+(198, 'SC', 'Seychelles'),
+(199, 'SL', 'Sierra Leona'),
+(200, 'SG', 'Singapur'),
+(201, 'SY', 'Siria'),
+(202, 'SO', 'Somalia'),
+(203, 'LK', 'Sri Lanka'),
+(204, 'SZ', 'Suazilandia'),
+(205, 'ZA', 'Sudáfrica'),
+(206, 'SD', 'Sudán'),
+(207, 'SE', 'Suecia'),
+(208, 'CH', 'Suiza'),
+(209, 'SR', 'Surinam'),
+(210, 'SJ', 'Svalbard y Jan Mayen'),
+(211, 'TH', 'Tailandia'),
+(212, 'TW', 'Taiwán'),
+(213, 'TZ', 'Tanzania'),
+(214, 'TJ', 'Tayikistán'),
+(215, 'IO', 'Territorio Británico del Océano Índico'),
+(216, 'TF', 'Territorios Australes Franceses'),
+(217, 'TL', 'Timor Oriental'),
+(218, 'TG', 'Togo'),
+(219, 'TK', 'Tokelau'),
+(220, 'TO', 'Tonga'),
+(221, 'TT', 'Trinidad y Tobago'),
+(222, 'TN', 'Túnez'),
+(223, 'TC', 'Islas Turcas y Caicos'),
+(224, 'TM', 'Turkmenistán'),
+(225, 'TR', 'Turquía'),
+(226, 'TV', 'Tuvalu'),
+(227, 'UA', 'Ucrania'),
+(228, 'UG', 'Uganda'),
+(229, 'UY', 'Uruguay'),
+(230, 'UZ', 'Uzbekistán'),
+(231, 'VU', 'Vanuatu'),
+(232, 'VE', 'Venezuela'),
+(233, 'VN', 'Vietnam'),
+(234, 'VG', 'Islas Vírgenes Británicas'),
+(235, 'VI', 'Islas Vírgenes de los Estados Unidos'),
+(236, 'WF', 'Wallis y Futuna'),
+(237, 'YE', 'Yemen'),
+(238, 'DJ', 'Yibuti'),
+(239, 'ZM', 'Zambia'),
+(240, 'ZW', 'Zimbabue');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recordatorio`
+--
+
+CREATE TABLE `recordatorio` (
+  `id_recordatorio` int(11) NOT NULL,
+  `fecha_inicio` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `fecha_final` date NOT NULL,
+  `id_registro` int(8) NOT NULL,
+  `id_categoria` int(8) NOT NULL,
+  `descripcion` text COLLATE utf8_unicode_ci NOT NULL,
+  `modo_de_pago` text COLLATE utf8_unicode_ci NOT NULL,
+  `frecuencia` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `monto` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `recordatorio`
+--
+
+INSERT INTO `recordatorio` (`id_recordatorio`, `fecha_inicio`, `fecha_final`, `id_registro`, `id_categoria`, `descripcion`, `modo_de_pago`, `frecuencia`, `monto`) VALUES
+(27, '2022-06-16 03:33:08', '2022-06-16', 9, 1, 'Senati', 'Pago en efectivo', 'Activo hasta el día indicado', '600.00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recordatorio_tipo`
+--
+
+CREATE TABLE `recordatorio_tipo` (
+  `id_categoria` int(11) NOT NULL,
+  `tipo` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `recordatorio_tipo`
+--
+
+INSERT INTO `recordatorio_tipo` (`id_categoria`, `tipo`) VALUES
+(1, 'Educación'),
+(2, 'Comida y comedor'),
+(3, 'Compras'),
+(4, 'Transporte'),
+(5, 'Personal'),
+(6, 'Familiar'),
+(7, 'Cuidado de la salud'),
+(8, 'Comunidad'),
+(9, 'Vacaciones'),
+(10, 'Pagos Online');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro`
+--
+
+CREATE TABLE `registro` (
+  `id_registro` int(11) NOT NULL,
+  `fullname` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `correo` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `id_pais` int(8) NOT NULL,
+  `foto` text COLLATE utf8_unicode_ci DEFAULT '\'nulo.png\''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `registro`
+--
+
+INSERT INTO `registro` (`id_registro`, `fullname`, `correo`, `password`, `username`, `id_pais`, `foto`) VALUES
+(9, 'Zaghit Prado Martinez', 'regtil@gmail.com', '7249ZagPrad', 'kirtt20', 173, 'mifoto.jpg'),
+(13, 'Carlos', 'Juan122@gmail.com', '123456789', 'Juancarlos', 13, 'samurai-head-with-oni-mask-free-vector.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tarjetas`
+--
+
+CREATE TABLE `tarjetas` (
+  `id_tarjeta` int(11) NOT NULL,
+  `id_registro` int(8) NOT NULL,
+  `numero_tarjeta` text COLLATE utf8_unicode_ci NOT NULL,
+  `titular_tarjeta` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `fecha_vencimiento` text COLLATE utf8_unicode_ci NOT NULL,
+  `cvv` int(3) NOT NULL,
+  `id_targ_mar` int(8) NOT NULL,
+  `id_moneda` int(8) NOT NULL,
+  `monto_ahorros` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `tarjetas`
+--
+
+INSERT INTO `tarjetas` (`id_tarjeta`, `id_registro`, `numero_tarjeta`, `titular_tarjeta`, `fecha_vencimiento`, `cvv`, `id_targ_mar`, `id_moneda`, `monto_ahorros`) VALUES
+(7, 9, '3658 6524 8965 4896', 'Zaghit Prado', '02 / 2050', 546, 4, 1, '300.00'),
+(8, 9, '3452 1456 2145 3214', 'Zaghit Aaron Prado M', '02 / 2040', 544, 3, 1, '49.99'),
+(17, 9, '4563 2145 8963 2145', 'Zaghit Aaron Prado M', '02 / 2050', 405, 1, 2, '449.00'),
+(18, 13, '4328 9582 5239 6841', 'Juan Carlo Peres', '12 / 2050', 123, 1, 1, '500.00'),
+(19, 9, '4625 5623 1265 2600', 'Zaghit Aaron Prado M', '02 / 2050', 154, 1, 1, '1000.50');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transferencia`
+--
+
+CREATE TABLE `transferencia` (
+  `id_tra` int(8) NOT NULL,
+  `id_registro` int(8) NOT NULL,
+  `num_cuenta` text COLLATE utf8_unicode_ci NOT NULL,
+  `id_moneda` int(8) NOT NULL,
+  `monto` decimal(10,2) NOT NULL,
+  `id_origen` int(8) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(8) NOT NULL,
+  `correo` text COLLATE utf8_unicode_ci NOT NULL,
+  `password` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `correo`, `password`) VALUES
+(1, 'regtil@gmail.com', '7249ZagPrad');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `marca_targeta`
+--
+ALTER TABLE `marca_targeta`
+  ADD PRIMARY KEY (`id_targ_mar`);
+
+--
+-- Indices de la tabla `monedas`
+--
+ALTER TABLE `monedas`
+  ADD PRIMARY KEY (`id_moneda`);
+
+--
+-- Indices de la tabla `notif`
+--
+ALTER TABLE `notif`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `paises`
+--
+ALTER TABLE `paises`
+  ADD PRIMARY KEY (`id_pais`);
+
+--
+-- Indices de la tabla `recordatorio`
+--
+ALTER TABLE `recordatorio`
+  ADD PRIMARY KEY (`id_recordatorio`),
+  ADD KEY `id_categoria` (`id_categoria`),
+  ADD KEY `id_registro` (`id_registro`);
+
+--
+-- Indices de la tabla `recordatorio_tipo`
+--
+ALTER TABLE `recordatorio_tipo`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `registro`
+--
+ALTER TABLE `registro`
+  ADD PRIMARY KEY (`id_registro`),
+  ADD KEY `id_pais` (`id_pais`);
+
+--
+-- Indices de la tabla `tarjetas`
+--
+ALTER TABLE `tarjetas`
+  ADD PRIMARY KEY (`id_tarjeta`),
+  ADD KEY `id_registro` (`id_registro`),
+  ADD KEY `id_targ_mar` (`id_targ_mar`),
+  ADD KEY `id_moneda` (`id_moneda`);
+
+--
+-- Indices de la tabla `transferencia`
+--
+ALTER TABLE `transferencia`
+  ADD PRIMARY KEY (`id_tra`),
+  ADD KEY `id_registro` (`id_registro`),
+  ADD KEY `id_origen` (`id_origen`),
+  ADD KEY `id_moneda` (`id_moneda`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `marca_targeta`
+--
+ALTER TABLE `marca_targeta`
+  MODIFY `id_targ_mar` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `monedas`
+--
+ALTER TABLE `monedas`
+  MODIFY `id_moneda` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `notif`
+--
+ALTER TABLE `notif`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `paises`
+--
+ALTER TABLE `paises`
+  MODIFY `id_pais` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=241;
+
+--
+-- AUTO_INCREMENT de la tabla `recordatorio`
+--
+ALTER TABLE `recordatorio`
+  MODIFY `id_recordatorio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT de la tabla `recordatorio_tipo`
+--
+ALTER TABLE `recordatorio_tipo`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `registro`
+--
+ALTER TABLE `registro`
+  MODIFY `id_registro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT de la tabla `tarjetas`
+--
+ALTER TABLE `tarjetas`
+  MODIFY `id_tarjeta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT de la tabla `transferencia`
+--
+ALTER TABLE `transferencia`
+  MODIFY `id_tra` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `recordatorio`
+--
+ALTER TABLE `recordatorio`
+  ADD CONSTRAINT `recordatorio_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `recordatorio_tipo` (`id_categoria`),
+  ADD CONSTRAINT `recordatorio_ibfk_2` FOREIGN KEY (`id_registro`) REFERENCES `registro` (`id_registro`);
+
+--
+-- Filtros para la tabla `registro`
+--
+ALTER TABLE `registro`
+  ADD CONSTRAINT `registro_ibfk_1` FOREIGN KEY (`id_pais`) REFERENCES `paises` (`id_pais`);
+
+--
+-- Filtros para la tabla `tarjetas`
+--
+ALTER TABLE `tarjetas`
+  ADD CONSTRAINT `tarjetas_ibfk_1` FOREIGN KEY (`id_registro`) REFERENCES `registro` (`id_registro`),
+  ADD CONSTRAINT `tarjetas_ibfk_2` FOREIGN KEY (`id_targ_mar`) REFERENCES `marca_targeta` (`id_targ_mar`),
+  ADD CONSTRAINT `tarjetas_ibfk_3` FOREIGN KEY (`id_moneda`) REFERENCES `monedas` (`id_moneda`);
+
+--
+-- Filtros para la tabla `transferencia`
+--
+ALTER TABLE `transferencia`
+  ADD CONSTRAINT `transferencia_ibfk_2` FOREIGN KEY (`id_registro`) REFERENCES `registro` (`id_registro`),
+  ADD CONSTRAINT `transferencia_ibfk_3` FOREIGN KEY (`id_origen`) REFERENCES `tarjetas` (`id_tarjeta`),
+  ADD CONSTRAINT `transferencia_ibfk_4` FOREIGN KEY (`id_moneda`) REFERENCES `monedas` (`id_moneda`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
