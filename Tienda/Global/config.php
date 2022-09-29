3cfcae94abc37feb0e0dacc1136386bf50f2958e
Zaghit-Prado <114609659+Zaghit-Prado@users.noreply.github.com>
2022-09-28 23:26:09 -0500

    Add files via upload

---
+++ Tienda/Global/config.php
@@ -0,0 +1,11 @@
+<?php
+define('SERVIDOR', 'localhost');
+define("USUARIO", "root");
+define("PASSWORD", "");
+define("BD", "tienda");
+?>
+<?php
+define("KEY", "72493800");
+define("COD", "AES-128-ECB");
+?>
+
